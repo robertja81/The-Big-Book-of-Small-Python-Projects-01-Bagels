670ef0537c7700b4a2b55073efdfa907421f1346
Bobby <robertja@gmail.com>
2021-11-10 20:50:25 -0600

    Implemented a function called get_input() that contains a basic loop that prompts for an answer.

--- main.py
+++ main.py
@@ -11,3 +11,28 @@
 # and “Bagels” if your guess has no correct digits.
 # You have 10 tries to guess the secret number.
 
+import random
+
+def get_input():
+    tries_remaining = 10
+    guess = '000'
+    guessed_correct = False
+    print("Please enter in a number between 1 and 100")
+    while tries_remaining > 0:
+        print("You have " + str(tries_remaining) + " guesses remaining")
+        tries_remaining -= 1
+        guess = input("Your guess?")
+        if guess == answer:
+            guessed_correct = True
+            print("You guessed correctly!")
+            tries_remaining = 0
+
+# Driver
+answer = random.randint(1, 100)
+
+get_input()
+
+
+
+
+
